57b4d706327b977c2eab41024cb6e34c1b64e3c2
Md Alauddin <alauddin.ahmed9282@gmail.com>
2025-09-21 16:15:44 +0600

    add expo routing

---
+++ app/(protected)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const ProtectedLayout = () => {
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
+
+export default ProtectedLayout;
